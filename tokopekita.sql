5ae6c48649dc4dfd32cc9f3531a546cc3c9bd700
ihsanzaki <54127828+ihsanzaki@users.noreply.github.com>
2022-02-10 15:23:29 +0700

    first commit

---
+++ tokopekita.sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Des 2021 pada 11.17
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokopekita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart`
+--
+
+CREATE TABLE `cart` (
+  `idcart` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tglorder` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(10) NOT NULL DEFAULT 'Cart'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `cart`
+--
+
+INSERT INTO `cart` (`idcart`, `orderid`, `userid`, `tglorder`, `status`) VALUES
+(10, '15wKVT0nej25Y', 2, '2020-03-16 12:10:42', 'Selesai'),
+(11, '15Swf8Ye0Fm.M', 2, '2020-03-16 12:17:34', 'Cart'),
+(12, '15PzF03ejd8W2', 1, '2020-05-13 02:40:48', 'Confirmed'),
+(13, '166cGizqrMgEQ', 3, '2021-12-06 13:11:13', 'Selesai');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detailorder`
+--
+
+CREATE TABLE `detailorder` (
+  `detailid` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `idproduk` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int(11) NOT NULL,
+  `namakategori` varchar(20) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Kesalahan membaca data untuk tabel tokopekita.kategori: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `tokopekita`.`kategori`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `idkonfirmasi` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `namarekening` varchar(25) NOT NULL,
+  `tglbayar` date NOT NULL,
+  `tglsubmit` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `userid` int(11) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgljoin` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(7) NOT NULL DEFAULT 'Member',
+  `lastlogin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`userid`, `namalengkap`, `email`, `password`, `notelp`, `alamat`, `tgljoin`, `role`, `lastlogin`) VALUES
+(1, 'Admin', 'admin', '$2y$10$GJVGd4ji3QE8ikTBzNyA0uLQhiGd6MirZeSJV1O6nUpjSVp1eaKzS', '01234567890', 'Indonesia', '2020-03-16 11:31:17', 'Admin', NULL),
+(2, 'Guest', 'guest', '$2y$10$xXEMgj5pMT9EE0QAx3QW8uEn155Je.FHH5SuIATxVheOt0Z4rhK6K', '01234567890', 'Indonesia', '2020-03-16 11:30:40', 'Member', NULL),
+(3, 'ihsan', 'syntaxc122@gmail.com', '$2y$10$ay8RiAdQWK3IR7sGx8KceOlywell3BuK.PNecHClx/SGuPpBLWn2e', '1234567890', 'jln.kota bambu', '2021-12-06 13:07:37', 'Member', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no` int(11) NOT NULL,
+  `metode` varchar(25) NOT NULL,
+  `norek` varchar(25) NOT NULL,
+  `logo` text DEFAULT NULL,
+  `an` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no`, `metode`, `norek`, `logo`, `an`) VALUES
+(1, 'Bank BCA', '13131231231', 'images/bca.jpg', 'Tokopekita'),
+(2, 'Bank Mandiri', '943248844843', 'images/mandiri.jpg', 'Tokopekita'),
+(3, 'DANA', '0882313132123', 'images/dana.png', 'Tokopekita');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int(11) NOT NULL,
+  `idkategori` int(11) NOT NULL,
+  `namaproduk` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `hargabefore` int(11) NOT NULL,
+  `hargaafter` int(11) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `idkategori`, `namaproduk`, `gambar`, `deskripsi`, `rate`, `hargabefore`, `hargaafter`, `tgldibuat`) VALUES
+(5, 4, 'kulkas LG Door in Door', 'produk/16PhkfJUYb56A.jpg', 'sajhgbjhgbn', 5, 15000000, 12000000, '2021-12-07 07:19:00'),
+(6, 5, 'kulkas panasonic Door in Door', 'produk/16zX04xeDiILk.png', 'sadfgfhgjgfedwfgdfdsfdgfdafgdfdsfgdfsd', 3, 7000, 5000, '2021-12-07 07:20:20'),
+(7, 4, 'kulkas LG Door in Door', 'produk/16wBrmqjfoiEw.png', 'sadfgfhgjgfedwfgdfdsfdgfdafgdfdsfgdfsd,jhgcxcv', 4, 9000, 8000, '2021-12-07 07:21:17'),
+(8, 4, 'kulkas LG Door in Door 4 pintu', 'produk/16wBrmqjfoiEw.png', 'sdfgbdsgdgfdfsgffdsdfhn', 47, 2147483647, 2147483647, '2021-12-07 07:41:14'),
+(9, 5, 'kulkas Panasonic Door by Door', 'produk/16zX04xeDiILk.png', 'Kulkas Panasonic mendukung gaya hidup sehat untuk menciptakan momen indah setiap hari. Pembekuan PrimeFresh -3ºC menjaga makanan tetap segar dan nutrisipun terjaga. Dan membuat persiapan memasak lebih', 10, 7600000, 6500000, '2021-12-07 10:11:31'),
+(10, 5, 'kulkas Panasonic 1 pintu', 'produk/16PhkfJUYb56A.jpg', 'Kulkas Panasonic mendukung gaya hidup sehat untuk menciptakan momen indah setiap hari. Pembekuan PrimeFresh -3ºC menjaga makanan tetap segar dan nutrisipun terjaga. Dan membuat persiapan memasak lebih', 10, 4000000, 3000000, '2021-12-07 10:13:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD UNIQUE KEY `orderid` (`orderid`),
+  ADD KEY `orderid_2` (`orderid`);
+
+--
+-- Indeks untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD PRIMARY KEY (`detailid`),
+  ADD KEY `orderid` (`orderid`),
+  ADD KEY `idproduk` (`idproduk`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indeks untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`idkonfirmasi`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`),
+  ADD KEY `idkategori` (`idkategori`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  MODIFY `detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `idkonfirmasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD CONSTRAINT `idproduk` FOREIGN KEY (`idproduk`) REFERENCES `produk` (`idproduk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `cart` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `login` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `idkategori` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
